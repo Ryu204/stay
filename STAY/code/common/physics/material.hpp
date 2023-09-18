6a6df361762cff2c2179ee5db211d93d54169a20
NguyenAnhBao <baotrass@gmail.com>
2023-09-19 01:17:17 +0700

    Add material

---
+++ STAY/code/common/physics/material.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <box2d/box2d.h>
+
+#include "../type/serializable.hpp"
+
+namespace stay
+{
+    namespace phys
+    {
+        struct Material : Serializable
+        {
+                // Unit: scalar
+                float friction{0.2F};
+                // Unit: kg/m^2
+                float density{0.F};
+                Json::Value toJSONObject() const override
+                {
+                    Json::Value res;
+                    res["friction"] = friction;
+                    res["density"]  = density;
+                }
+                bool fetch(const Json::Value& data) override
+                {
+                    if (data.isObject() && data["friction"].isNumeric() && data["density"].isNumeric())
+                    {
+                        friction = data["friction"].asFloat();
+                        density = data["density"].asFloat();
+                        return true;
+                    }
+                    return false;
+                }
+            private:
+                // Fetch the data into `fixture`
+                // @note This function will not change friction of existing contacts
+                void updateB2dFixture(b2Fixture* fixture)
+                {
+                    fixture->SetFriction(friction);
+                    fixture->SetDensity(density);
+                    fixture->GetBody()->ResetMassData();
+                }
+        };
+    } // namespace phys
+} // namespace stay
--- STAY/code/common/program/main.cpp
+++ STAY/code/common/program/main.cpp
@@ -101,4 +101,93 @@ int systemRegistration()
     return 1;
 }
 // test==================================================================
+#include <box2d/box2d.h>
+
+int b2dHelloWorld()
+{
+    b2Vec2 gravity(0.F, -9.8F);
+    b2World world(gravity);
+
+    b2BodyDef groundBodyDef;
+    groundBodyDef.position.Set(0.F, -10.F);
+    auto* groundBody = world.CreateBody(&groundBodyDef);
+
+    b2PolygonShape box;
+    box.SetAsBox(50.F, 10.F);
+
+    groundBody->CreateFixture(&box, 0.F);
+
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(0.F, 40.F);
+    auto* body = world.CreateBody(&bodyDef);
+    b2PolygonShape bodyShape;
+    bodyShape.SetAsBox(1.F, 1.F);
+    b2FixtureDef fixtureDef;
+    fixtureDef.density = 1.F;
+    fixtureDef.friction = 0.3F;
+    fixtureDef.shape = &bodyShape;
+    body->CreateFixture(&fixtureDef);
+
+    int32 velocityIterations = 6;
+    int32 positionIterations = 2;
+
+    sf::RectangleShape ground(sf::Vector2f{100.F, 20.F});
+    sf::RectangleShape player(sf::Vector2f{10.F, 10.F});
+
+    ground.setFillColor(sf::Color(0x008800FF));
+    ground.setOutlineColor(sf::Color::Green);
+    ground.setOutlineThickness(-1.F);
+    ground.setPosition(0, -10);
+
+    player.setFillColor(sf::Color(0x990000FF));
+    player.setOutlineColor(sf::Color::Red);
+    player.setOutlineThickness(-1.F);
+
+    RWin win(sf::VideoMode(60, 60), "b2d");
+
+    sf::Clock clock;
+    float elapsed = 0.F;
+
+for (int32 i = 0; i < 60; ++i)
+{
+    world.Step(1.F / 60.F, velocityIterations, positionIterations);
+    b2Vec2 position = body->GetPosition();
+    float angle = body->GetAngle();
+    printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+}
+
+    while (win.isOpen())
+    {
+        elapsed += clock.restart().asSeconds();
+        while (elapsed > 1.F / 60.F)
+        {
+            elapsed -= 1.F / 60.F;
+            sf::Event e;
+            while (win.pollEvent(e))
+            {
+                switch (e.type)
+                {
+                case sf::Event::Closed:
+                    win.close();
+                    break;
+                default:
+                    break;
+                }
+            }
+            // update
+            world.Step(1.F / 60.F, velocityIterations, positionIterations);
+            player.setPosition(body->GetPosition().x, body->GetPosition().y);
+            player.setRotation(body->GetAngle() * 180 / 3.14);
+        }
+        // render
+        win.clear();
+        win.draw(player);
+        win.draw(ground);
+        win.display();
+    }
+
+    return 0;
+}
+// test==================================================================
 // NOLINTEND
