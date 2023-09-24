2a97555577ca6090f7271f70df6b8d37cfa3d1bd
NguyenAnhBao <baotrass@gmail.com>
2023-09-25 00:56:19 +0700

    Remove uneccessary param

--- STAY/code/common/utility/IDgen.hpp
+++ STAY/code/common/utility/IDgen.hpp
@@ -9,7 +9,6 @@ namespace stay
 		// Manage ID creation and deletion
 		// N is the number of maximum available IDs
         // This class asserts excessively in debug build if the parameters are faulty
-		template <std::size_t N>
 		class IDGenerator
 		{
 		public:
