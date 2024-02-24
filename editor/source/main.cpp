6af595a82095b22bb32691ce64738f7c030980db
NguyenAnhBao <baotrass@gmail.com>
2024-02-24 20:13:31 +0700

    build imgui example

--- editor/source/main.cpp
+++ editor/source/main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 
-#include <stay/program/application.hpp>
+#include <SFML/Graphics.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
+
+#include "theme.hpp"
 
 int main()
 {
     try
     {
-        stay::program::Application app;
-        app.run();
+        sf::RenderWindow window{sf::VideoMode{500, 600}, "hi"};
+        if (!ImGui::SFML::Init(window))
+            throw std::runtime_error{"Cannot init windows with imgui"};
+        sf::Clock clock;
+        while (window.isOpen())
+        {
+            sf::Event event;
+            ImGui::SFML::ProcessEvent(window, event);
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+            ImGui::SFML::Update(window, clock.restart());
+            ImGui::ShowDemoWindow();
+            ImGui::Begin("Hello, world!");
+            ImGui::Button("Look at this pretty button");
+            ImGui::End();
+
+            window.clear(primaryBackground);
+            ImGui::SFML::Render(window);
+            window.display();
+        }
+        ImGui::SFML::Shutdown();
         return 0;
     }
     catch (std::exception& e)
---
+++ editor/source/theme.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stay/type/color.hpp>
+
+inline const stay::Color primaryBackground{0x68747DFF};
